17ab1b65e3246f410df01325b1cf51701a205b97
sooki88 <sookhee9n@gmail.com>
2024-01-04 14:07:55 +0900

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/products/:id",
+        destination: "/items/:id",
+        permanent: true, //웹브라우저에게 주소가 바뀌었다는 걸 저장하려면 true, 아니면 false
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/codeitmall/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
+import "@/styles/global.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Codeitmall</title>
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
